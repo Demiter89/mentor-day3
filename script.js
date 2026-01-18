637258e18a2a01b39007fe9087b3f5a4499f840c
Maksym Shevchuk <maksymshevchuk89@gmai.com>
2026-01-18 16:55:12 +0100

    Add contact form and fix responsive + JS selector

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ navLinks.forEach(link => {
         })
     })
 })
+
+const form = document.querySelector('#contact-form');
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const email = formData.get('email');
+    console.log('Name:', name);
+    console.log('Emeil:', email);
+    form.reset();
+
+});
